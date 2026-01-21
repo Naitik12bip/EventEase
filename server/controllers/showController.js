958b3c8dc346cbb1529ac02ef2df593d5887c693
Naitik <naitikporiya2@gmail.com>
2026-01-21 23:26:31 +0530

    fix

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+import Movie from "../models/Movie";
 
+// API to get now playing movies from TMDB
 export const getNowPlayingMovies = async (req, res) => {
     try {
         const { data } = await axios.get("https://api.themoviedb.org/3/movie/now_playing", {
@@ -12,6 +14,23 @@ export const getNowPlayingMovies = async (req, res) => {
         res.json({ success: true, movies: movies });
     } catch (error) {
         console.error(error);
-        res.json({success: false, message: error.message})
+        res.json({ success: false, message: error.message })
+    }
+}
+
+//API to add a new show to the database 
+export const addShow = async (req, res) => {
+    try {
+        const { movieId, showsInput, showPrice } = req.body;
+
+        let movie = await Movie.findById(movieId);
+        if (!movie) {
+            // fetch movie details and credits from TMDB API
+            return
+            return
+        }
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message })
     }
 }
