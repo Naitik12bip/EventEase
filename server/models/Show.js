a3af4947de4243eddeaedb5cd56b314d1e297081
Naitik <naitikporiya2@gmail.com>
2026-01-21 22:09:52 +0530

    fix of bugs

--- server/models/Show.js
+++ server/models/Show.js
@@ -2,12 +2,20 @@ import mongoose from "mongoose";
 
 const showSchema = new mongoose.Schema(
     {
-    movie: { type: String, required: true, ref: 'Movie' },
-    showDateTime: { type: Date, required: true },
-    showPrice: { type: Number, required: true },
-    occupiedSeats: { type: Object, default: {} },
-    }, {minimize: false}
-)
+        // FIX: Changed type to ObjectId to work with 'ref'
+        movie: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            required: true, 
+            ref: 'Movie' 
+        },
+        showDateTime: { type: Date, required: true },
+        showPrice: { type: Number, required: true },
+        // NOTE: Using Map or a nested Schema is often safer for objects, 
+        // but {minimize: false} ensures this empty object is saved.
+        occupiedSeats: { type: Object, default: {} },
+    }, 
+    { minimize: false, timestamps: true } // Added timestamps for better record keeping
+);
 
 const Show = mongoose.model("Show", showSchema);
 
